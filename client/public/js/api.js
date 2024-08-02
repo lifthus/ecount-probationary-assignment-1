951f284ddf3d9ace435aa2938d2de4ddbf6ab54a
Arang luv <ruhunsu3@naver.com>
2024-08-02 13:34:06 +0900

    코드 리펙토링

--- client/public/js/api.js
+++ client/public/js/api.js
@@ -4,7 +4,7 @@ const URL = "http://172.29.12.20:8000";
 // 수익 및 지출 데이터를 받아옵니다
 export const getIncomeInfo = async (year, month) => {
   try {
-    const response = await fetch(`${URL}/cashflow?year=${year}&month=08`)
+    const response = await fetch(`${URL}/cashflow?year=${year}&month=${month}`)
     if (!response.ok) throw new Error("수입/ 지출을 받아오는데 문제가 발생했습니다")
     const data  = await response.json(); 
     return data
@@ -32,6 +32,7 @@ export const postUserData = async (data) => {
     if (!response.ok) {
       throw new Error("수입/ 지출을 입력하는데 문제가 발생했습니다");
     }
+    return response.json();
   } catch (e) {
     console.log(e);
     alert(e.message);
--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,147 +1,47 @@
-import { getIncomeInfo, postUserData } from "./api.js";
+import * as mainAction from "./action.js";
 
+let cashflow = null;
 // 받은 데이터를 전역에서 다룹니다
 const select = document.querySelector(".toggle_form select");
-const [initYear, initMonth] = select.value.split(".");
-const { data } = await getIncomeInfo(initYear, initMonth);
-
-console.log("data")
-console.log(data)
-// 초기 income data를 받아옵니다
-try {
-  if (!data) {
-    // thers is no user data
-  } 
-  
-  const tbody = document.querySelector("#income-tbody")
-  for (let item of data) {
-    const {date, asset_type, classification, content, price} = item;
-    const year = new Date(date).getFullYear();
-    const month = new Date(date).getMonth();
-    
-    const tr = document.createElement("tr");
-    const dateTd = document.createElement("td");
-    const assetTypeTd = document.createElement("td");
-    const classificationTd = document.createElement("td");
-    const contentTd = document.createElement("td");
-    const priceTd = document.createElement("td");
-    
-    dateTd.append(`${year}.${month}`);
-    assetTypeTd.append(asset_type);
-    classificationTd.append(classification);
-    contentTd.append(content);
-    priceTd.append(price);
-
-    tr.append(dateTd);
-    tr.append(assetTypeTd);
-    tr.append(classificationTd);
-    tr.append(contentTd);
-    tr.append(priceTd);
-    // for (const child of values) {
-    //   const td = document.createElement("td");
-    //   td.append(child);
-    //   tr.append(td);
-    // }
-    tbody.appendChild(tr);
-  }
-} catch (e) {
-  console.log(`getIncomeData error : ${e}`)
-}
-
-// 수입 / 지출 / 총합을 화면에 그립니다
 const totalIncomeSpan = document.querySelector("#total-income");
 const incomeSpan = document.querySelector("#income-price");
 const paymentSpan = document.querySelector("#payment-price");
-let totalIncome = 0;
-let income = 0;
-let payment = 0;
-for (const item of data) {
-  const {price} = item;
-  let num = parseInt(price)
+const form = document.getElementById("user-form");
 
-  if (num < 0) {
-    totalIncome += num;
-    payment += Math.abs(num);
-  }
-  if (num > 0) {
-    totalIncome+= num;
-    income += num;
-  }
-}
-
-totalIncomeSpan.innerHTML = totalIncome;
-incomeSpan.innerHTML = income;
-paymentSpan.innerHTML = payment;
+mainAction.init(select)
+  .then((data) => cashflow = data)
+  .then(() => mainAction.handleTotalIncome(cashflow, totalIncomeSpan, incomeSpan, paymentSpan));
 
 // 사용자 수입 / 지출 onSubmit handler
-const form = document.getElementById("user-form");
-form.onsubmit = (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const formData = new FormData(form);
-  let price = formData.get("action-type") === "지출" ? parseInt(formData.get("price")) * -1 : parseInt(formData.get("price"))
-  formData.set("price", price)
-  formData.delete("action-type");
-  // 데이터 post
-  const data = {
-    cashflow: {
-
-    }
-  };
-  formData.forEach((value, key) => {
-    console.log(`${key}: ${value}`);
-    data.cashflow[key] = value;
+form.addEventListener("submit",  async (e) => {
+   await mainAction.userSubmitHandler(e)
+   mainAction.loadData(select).then((data) => {
+    cashflow = data;
+    mainAction.handleTableRendering(cashflow);
+    mainAction.handleTotalIncome(cashflow, totalIncomeSpan, incomeSpan, paymentSpan)
+  })
 });
-
-
-  postUserData(data);
-  // 폼 데이터를 콘솔에 출력  
-}
-// action-type: 지출
-// date: 2024-08-09
-// asset-type: 토스뱅크
-// classification: 급여
-// price: 2800000
-// content: 수입
-
-// 수입 & 지출 toggle part
-
-const incomeLabel = document.getElementById("income-label");
-const paymentLabel = document.getElementById("payment-label");
-
-incomeLabel.onclick = (e) => {
-  console.log("income label click")
-  console.log(e.target);
-}
-
-paymentLabel.onclick = (e) => {
-  console.log("payment label click")
-  console.log(e.target);
-}
 
 // toggle left click part
 const leftBtn = document.querySelector('.toggle_form input[value="⬅"]')
-console.log("leftBtn")
-console.log(leftBtn)
-const handleLeftClick = () => {
-  console.log("click")
-  if (select.selectedIndex === 0) {
-    return;
-  }
-  select.selectedIndex -= 1;
-  return;
-}
-leftBtn.addEventListener("click", handleLeftClick);
+leftBtn.addEventListener("click", () => mainAction.togglePrevCashflow(select));
+leftBtn.addEventListener("click", () => {
+  // const newData = mainAction.loadData(select);
+  mainAction.loadData(select).then((data) => {
+    cashflow = data;
+    mainAction.handleTableRendering(cashflow);
+    mainAction.handleTotalIncome(cashflow, totalIncomeSpan, incomeSpan, paymentSpan)
+  })
+});
 
-// toggle right click part
-
+// toggle rigth click part
 const rigthBtn = document.querySelector('.toggle_form input[value="➡"]')
-const handleRigthClick = () => {
-  console.log("click")
-  if (select.selectedIndex === select.options.length - 1) {
-    return;
-  }
-  select.selectedIndex += 1;
-  return;
-}
-rigthBtn.addEventListener("click", handleRigthClick);
+rigthBtn.addEventListener("click", (e) => mainAction.toggleRigthCashflow(select));
+rigthBtn.addEventListener("click", () => {
+  // const newData = mainAction.loadData(select);
+  mainAction.loadData(select).then((data) => {
+    cashflow = data;
+    mainAction.handleTableRendering(cashflow);
+    mainAction.handleTotalIncome(cashflow, totalIncomeSpan, incomeSpan, paymentSpan)
+  })
+})
